742f850ba54865ee8a79645d635f322ec698eb48
Gabriel Assed <gabriel.assed@otima.digital>
2026-01-12 08:05:48 -0300

    format using prettier

--- undisposition_bg.js
+++ undisposition_bg.js
@@ -1,65 +1,69 @@
-var active = true
+var active = true;
 if (typeof browser === 'undefined') {
-  var browser = chrome
+  var browser = chrome;
 }
 
 function loadOptions(callback) {
   browser.storage.local.get('activeStatus', function (data) {
     if (data.activeStatus === undefined) {
       //at first install
-      data.activeStatus = true
-      saveOptions()
+      data.activeStatus = true;
+      saveOptions();
     }
-    active = data.activeStatus
-    if (callback != null) callback()
-  })
+    active = data.activeStatus;
+    if (callback != null) callback();
+  });
 }
 
 function saveOptions() {
-  browser.storage.local.set({ activeStatus: active })
+  browser.storage.local.set({ activeStatus: active });
 }
 
 function updateUI() {
-  var str = active ? 'Undisposition active, click to deactivate' : 'Undisposition disabled, click to activate'
-  browser.action.setTitle({ title: str })
-  browser.action.setBadgeText({ text: active ? '⠀' : '⠀' })
-  browser.action.setBadgeBackgroundColor({ color: active ? '#5084ee' : '#e91e63' })
+  var str = active
+    ? 'Undisposition active, click to deactivate'
+    : 'Undisposition disabled, click to activate';
+  browser.action.setTitle({ title: str });
+  browser.action.setBadgeText({ text: active ? '⠀' : '⠀' });
+  browser.action.setBadgeBackgroundColor({
+    color: active ? '#5084ee' : '#e91e63',
+  });
 
   if (active) {
-    setDynamicRule()
+    setDynamicRule();
   } else {
-    removeDynamicRules()
+    removeDynamicRules();
   }
 }
 
 function ToggleActive() {
-  active = !active
-  saveOptions()
-  updateUI()
+  active = !active;
+  saveOptions();
+  updateUI();
 }
 
 async function localGetSync(key) {
   return new Promise((resolve, reject) => {
     try {
       browser.storage.local.get(key, function (value) {
-        resolve(value[key])
-      })
+        resolve(value[key]);
+      });
     } catch (ex) {
-      reject(ex)
+      reject(ex);
     }
-  })
+  });
 }
 
 function removeDynamicRules() {
   // remove all rules
   browser.declarativeNetRequest.updateDynamicRules({
     removeRuleIds: [1],
-  })
+  });
 }
 
 async function setDynamicRule() {
-  const headerKey = 'content-disposition'
-  let list = await localGetSync('blacklist')
+  const headerKey = 'content-disposition';
+  let list = await localGetSync('blacklist');
   browser.declarativeNetRequest.updateDynamicRules({
     addRules: [
       {
@@ -82,30 +86,30 @@ async function setDynamicRule() {
       },
     ],
     removeRuleIds: [1],
-  })
+  });
 }
 
 // init everything
-loadOptions(updateUI)
-browser.action.onClicked.addListener(ToggleActive)
+loadOptions(updateUI);
+browser.action.onClicked.addListener(ToggleActive);
 
 // set right click menu to extension icon
 browser.contextMenus.create({
   id: 'settings',
   title: 'Settings',
   contexts: ['browser_action', 'action'],
-})
+});
 
 //handle context menu actions
 browser.contextMenus.onClicked.addListener(function (e) {
   if (e.menuItemId == 'settings') {
     browser.tabs.create({
       url: 'pages/settings.html',
-    })
+    });
   }
-})
+});
 
 // handle messages from content scripts
 chrome.runtime.onMessage.addListener(function () {
-  updateUI()
-})
+  updateUI();
+});
