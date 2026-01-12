ef23942d4c5ffd658f7a656c26769cbe7c060391
Gabriel Assed <gabriel.assed@otima.digital>
2026-01-12 08:20:11 -0300

    format

--- pages/settings.js
+++ pages/settings.js
@@ -1,61 +1,62 @@
 if (typeof browser === 'undefined') {
-  var browser = chrome
+  var browser = chrome;
 }
 
 function loadData() {
   browser.storage.local.get('blacklist', function (data) {
     if (data.blacklist === undefined) {
-      data.blacklist = []
+      data.blacklist = [];
     }
-    document.getElementById('blacklist').value = data.blacklist?.join('\n')
-  })
+    document.getElementById('blacklist').value = data.blacklist?.join('\n');
+  });
 }
 
 function _submitHandler() {
-  let list = document.getElementById('blacklist').value.split('\n')
-  list = list.map(e => trimHost(e))
-  list = list.filter(e => e)
+  let list = document.getElementById('blacklist').value.split('\n');
+  list = list.map((e) => trimHost(e));
+  list = list.filter((e) => e);
   browser.storage.local.set({ blacklist: list }, function () {
     // update script rules
     browser.runtime.sendMessage({}, function (response) {
-      console.log(response)
-    })
-  })
-  loadData()
+      console.log(response);
+    });
+  });
+  loadData();
 }
 
 function _testHandler() {
-  let list = document.getElementById('blacklist').value.split('\n')
-  list = list.map(e => e.trim())
+  let list = document.getElementById('blacklist').value.split('\n');
+  list = list.map((e) => e.trim());
 
-  let host = trimHost(document.getElementById('test').value)
-  host = 'http://' + host
+  let host = trimHost(document.getElementById('test').value);
+  host = 'http://' + host;
 
-  let results = []
+  let results = [];
 
-  list.forEach(e => {
+  list.forEach((e) => {
     if (host?.includes(e)) {
-      results.push(e)
+      results.push(e);
     }
-  })
+  });
 
   if (results.length > 0) {
-    document.getElementById('testResult').innerHTML = '<br/>Found matches:<br/><br/>' + results.join('<br />\n')
+    document.getElementById('testResult').innerHTML =
+      '<br/>Found matches:<br/><br/>' + results.join('<br />\n');
   } else {
-    document.getElementById('testResult').innerHTML = '<br/>No match'
+    document.getElementById('testResult').innerHTML = '<br/>No match';
   }
 }
 
 function trimHost(host) {
-  host = host.trim()
+  host = host.trim();
   if (host.startsWith('http')) {
-    host = host.split('/')[2]
+    host = host.split('/')[2];
   }
-  return host
+  return host;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('submit').onclick = _submitHandler
-  document.getElementById('testButton').onclick = _testHandler
-  loadData()
-})
+  document.getElementById('submit').onclick = _submitHandler;
+  document.getElementById('testButton').onclick = _testHandler;
+  loadData();
+});
